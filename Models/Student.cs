e81bbde057aea00a75aa4dff75f1b959d0211c75
Khanh Ho <khanhho7268@gmail.com>
2021-03-23 18:30:19 -0400

    Initial Commit

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab6.Models
+{
+    public class Student : StudentBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "VIN Number")]
+        public Guid ID { get; set; }
+    }
+}
---
+++ Models/StudentBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab6.Models
+{
+    public class StudentBase
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Program { get; set; }
+    }
+}
